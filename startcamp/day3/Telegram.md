d93637c8b8988b3954eca410c41eaac5d47cbfe9
Gwanghun Im <lgh82229047@gmail.com>
2021-01-15 15:51:20 +0900

    새로운 업데이트

---
+++ startcamp/day3/Telegram.md
@@ -0,0 +1,14 @@
+## Telegram 자동화 챗봇 만들기
+
+> 특정 시간이 되면 나에게 필요한 정보를 전달해주는 봇
+
+### 가입 &로그인 
+
+- `데스크탑 앱` 권장
+- BotFather
+
+- **/newbot**
+
+- 이름설정
+  1. yeosusaram
+  2. yeosusaram_bot
---
+++ startcamp/day3/send_message.py
@@ -0,0 +1,22 @@
+# 가장 최근에 메시지를 보낸 사용자에게
+# 메시지 보내기 
+
+
+
+# 1. 요청에 필요한 라이브러리 import
+import requests, pprint
+
+# 2. API 요청 URL과 + Token 확인 
+token = '1541699096:AAHnOcfQrygewM2Xt_G9_UMNOwQWHdcLvD4'
+api_url = f'https://api.telegram.org/bot{token}'
+
+# 3. 메시지 보낸 사용자의 ID값 찾기
+chat_id_url = f'{api_url}/getUpdates'
+res = requests.get(chat_id_url).json()
+# pprint.pprint(res)
+chat_id = res['result'][0]['message']['chat']['id']
+print(chat_id)
+
+# 4. chat id 에게 메시지 보내기
+
+
